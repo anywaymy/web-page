828a666292528f9aaa5250c3108d462c2f2fccc7
anywaymy <simpletop4ik@gmail.com>
2026-03-28 12:14:43 +0300

    эндпоинт на обработку get запроса
    обычный эндпоинт, который отображает наш шаблон

--- core/main.py
+++ core/main.py
@@ -1,7 +1,34 @@
 import os
+import logging
 from pathlib import Path
+from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("web.log", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
+
+app = FastAPI()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+message_counter = 0
+
+
+@app.get("/", response_class=HTMLResponse)
+async def get_index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={}
+    )
+
